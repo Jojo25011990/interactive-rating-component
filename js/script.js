3391721894d2c74cecd92ee5c0c0f9e9b45f1014
Jozef Kudrna <jojo25011990@gmail.com>
2024-12-06 11:55:17 +0100

    added new solution javascript code

--- js/script.js
+++ js/script.js
@@ -6,8 +6,17 @@ const cardPopup = document.querySelector('.card__popup');
 const ratingInputs = document.querySelectorAll('.card__form-rating-input');
 const ratingPopup = document.querySelector('.card__popup-rating');
 
+// ------------------------------------------
+// New Solution with validation
+// to prevent action when nothing is selected
+// ------------------------------------------
+
 // *** Event Listener on button ***
 btn.addEventListener('click', () => {
+  // *** Check if any rating input is checked ***
+  const hasChecked = [...ratingInputs].find(input => input.checked);
+  if (!hasChecked) return; // *** Exit if nothing is selected ***
+
   // *** Remove the hidden class to show the popup card ***
   cardPopup.classList.remove('hidden');
 
@@ -15,11 +24,7 @@ btn.addEventListener('click', () => {
   card.classList.add('hidden');
 
   // *** Set the selected rating input value in the popup ***
-  ratingInputs.forEach(input => {
-    if (input.checked) {
-      ratingPopup.textContent = input.value;
-    }
-  });
+  ratingPopup.textContent = hasChecked.value;
 
   // *** My addition 😂 ***
   /*
@@ -31,3 +36,34 @@ btn.addEventListener('click', () => {
     card.classList.remove('hidden');
   }, 3000);
 });
+
+// ------------------------------------------
+// Old Solution without validation
+// to prevent action when nothing is selected
+// ------------------------------------------
+
+// *** Event Listener on button ***
+// btn.addEventListener('click', () => {
+//   // *** Remove the hidden class to show the popup card ***
+//   cardPopup.classList.remove('hidden');
+
+//   // *** Add the hidden class to hide the main card ***
+//   card.classList.add('hidden');
+
+//   // *** Set the selected rating input value in the popup ***
+//   ratingInputs.forEach(input => {
+//     if (input.checked) {
+//       ratingPopup.textContent = input.value;
+//     }
+//   });
+
+//   // *** My addition 😂 ***
+//   /*
+//   Since the challenge doesn't include a close btn
+//   , I added a timeout to close it
+//  */
+//   setTimeout(() => {
+//     cardPopup.classList.add('hidden');
+//     card.classList.remove('hidden');
+//   }, 3000);
+// });
